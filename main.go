f9629df01224d8d13dc21dbb8a20edc1e2db5512
Shadowbeetle <tamas.kadlecsik@gmail.com>
2019-03-01 20:35:57 +0100

    reformat

--- main.go
+++ main.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "os/exec"
-    "strings"
-    "github.com/atotto/clipboard"
+	"bufio"
+	"fmt"
+	"github.com/atotto/clipboard"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter the string to be cut: ")
-    text, _ := reader.ReadString('\n')
-    
-    text = strings.TrimSuffix(text, "\n")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the string to be cut: ")
+	text, _ := reader.ReadString('\n')
 
-    slicesOfFour := createSlices(text, 4)
+	text = strings.TrimSuffix(text, "\n")
 
-    setupTtyToReadChars()
-    getUserInputForChunks(slicesOfFour, 0)
+	slicesOfFour := createSlices(text, 4)
+
+	setupTtyToReadChars()
+	getUserInputForChunks(slicesOfFour, 0)
 }
 
-func createSlices (text string, n int) []string {
-    inputRunes := []rune(text)
-  
-    slicesOfFour := []string{}
-    substr := ""
-    for i, r := range inputRunes {
-        substr = substr + string(r)
-        if (i + 1) % n == 0 {
-          slicesOfFour = append(slicesOfFour, substr)
-          substr = ""
-        } else if (i + 1) == len(inputRunes) {
-          slicesOfFour = append(slicesOfFour, substr)
-        }
-    }
+func createSlices(text string, n int) []string {
+	inputRunes := []rune(text)
 
-    return slicesOfFour
+	slicesOfFour := []string{}
+	substr := ""
+	for i, r := range inputRunes {
+		substr = substr + string(r)
+		if (i+1)%n == 0 {
+			slicesOfFour = append(slicesOfFour, substr)
+			substr = ""
+		} else if (i + 1) == len(inputRunes) {
+			slicesOfFour = append(slicesOfFour, substr)
+		}
+	}
+
+	return slicesOfFour
 }
 
-func setupTtyToReadChars () {
-    exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-    exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+func setupTtyToReadChars() {
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
 }
 
-func getUserInputForChunks (slicesOfFour []string, idx int) {
-    for i, chunk := range slicesOfFour[idx:] {
-      fmt.Println("Next chunk: " + chunk)
-        fmt.Print("Would you like to (c)opy (s)kip or (e)xit [c/s/e]")
+func getUserInputForChunks(slicesOfFour []string, idx int) {
+	for i, chunk := range slicesOfFour[idx:] {
+		fmt.Println("Next chunk: " + chunk)
+		fmt.Print("Would you like to (c)opy (s)kip or (e)xit [c/s/e]")
 
-        reader := bufio.NewReader(os.Stdin)
-        char, _, _ := reader.ReadRune()
+		reader := bufio.NewReader(os.Stdin)
+		char, _, _ := reader.ReadRune()
 
-        fmt.Println("") 
-        
-        switch char {
-          case 'c':
-            clipboard.WriteAll(chunk)
-          case 's':
-            continue
-          case 'e':
-            os.Exit(0)
-          default:
-            fmt.Println("Invalid input, please try again")
-            getUserInputForChunks(slicesOfFour, i)
-        }
-    }
+		fmt.Println("")
+
+		switch char {
+		case 'c':
+			clipboard.WriteAll(chunk)
+		case 's':
+			continue
+		case 'e':
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid input, please try again")
+			getUserInputForChunks(slicesOfFour, i)
+		}
+	}
 }
